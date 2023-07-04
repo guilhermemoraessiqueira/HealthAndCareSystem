d63e1bebc6f95b34ae2b86815ee575108f36e8cd
gmoraes <112905369+guilhermemoraessiqueira@users.noreply.github.com>
2023-07-04 16:38:48 -0300

    first commit

---
+++ src/main/java/com/system/care/health/HealthAndCareSystem/HealthAndCareSystemApplication.java
@@ -0,0 +1,13 @@
+package com.system.care.health.HealthAndCareSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAndCareSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAndCareSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/care/health/HealthAndCareSystem/HealthAndCareSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.care.health.HealthAndCareSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAndCareSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
